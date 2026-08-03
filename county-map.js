23eed1c6c016b1cb20b935ef7ce6e459dd4e11fd
ElliottIan397 <IanElliott@EandSsolutions.com>
2026-08-03 01:01:40 -0500

    Update county-map.js
    fix format issue in legend

--- county-map.js
+++ county-map.js
@@ -51,7 +51,7 @@ L.geoJSON(window.mapData.commercialCore.geometry, {
         layer.bindTooltip(`
             <b>${window.mapData.commercialCore.county} Commercial Core</b><br>
             Radius: ${window.mapData.commercialCore.commercial_core_radius_miles} miles<br>
-            window.mapData.commercialCore.commercial_core_enterprises.toLocaleString()
+            Businesses: ${window.mapData.commercialCore.commercial_core_enterprises.toLocaleString()}
             (${window.mapData.commercialCore.commercial_core_enterprise_pct}%)<br>
             Employees: ${window.mapData.commercialCore.commercial_core_employees.toLocaleString()}
             (${window.mapData.commercialCore.commercial_core_employee_pct}%)<br>
